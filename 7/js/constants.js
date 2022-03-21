008a04ba86e6d3c92278050b442a020e374154ce
Keks <keks@htmlacademy.ru>
2022-03-21 15:39:37 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/constants.js
@@ -0,0 +1,46 @@
+
+const BOOKING_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const BookingType = {
+  FLAT: 'Квартира',
+  BUNGALOW: 'Бунгало',
+  HOUSE: 'Дом',
+  PALACE: 'Дворец',
+  HOTEL: 'Отель'
+};
+
+const CHECKIN_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FACILITIES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const Price = {
+  MIN: 1500,
+  MAX: 4000
+};
+
+const OFFERS_COUNT = 5;
+
+export {BOOKING_TYPES, CHECKIN_TIMES, FACILITIES, PHOTOS, Price, BookingType, OFFERS_COUNT};
---
+++ 7/js/util.js
@@ -0,0 +1,51 @@
+const getRandomInteger = (min, max) => {
+  if (!(Number.isInteger(min) && Number.isInteger(max))) {
+    return 'Числа должны быть целыми!';
+  }
+  if ((min < 0) || (max < 0)) {
+    return 'Введите положительные числа';
+  }
+
+  if (min === max) {
+    return 'Введите разные числа';
+  }
+
+  if  (min > max) {
+    return 'Неверный диапазон. Значение "от" больше значения "до"';
+  }
+
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const getRandomFloat = (min, max, float) => {
+  if ((min < 0) || (max < 0)) {
+    return 'Введите положительные числа';
+  }
+
+  if (min === max) {
+    return 'Введите разные числа';
+  }
+
+  if  (min > max) {
+    return 'Неверный диапазон. Значение "от" больше значения "до"';
+  }
+
+  return (Math.random() * (max - min) + min).toFixed(float);
+};
+
+const getAuthorAvatar = (avatarNumber) => (
+  `img/avatars/user${avatarNumber.padStart(2, '0')}.png`
+);
+
+const getListElements = (maxCount, items) => {
+  const maxElement = getRandomInteger(0, maxCount);
+  const allItems = [];
+
+  for (let i = 0; i <= maxElement; i++) {
+    allItems.push(items[i]);
+  }
+
+  return allItems;
+};
+
+export {getRandomInteger, getRandomFloat, getAuthorAvatar, getListElements};
